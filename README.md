8dad6f7228dec664645a95c2f8d4f04d430191ee
dan brackenbury <daniel.brackenbury@gmail.com>
2022-07-31 19:23:53 -0400

    added multi-day support, revamped save system, added jQueryUI, edited readme

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Work Day Scheduler
 ## Dan Brackenbury
-### Assembled with CSS, HTML, and Javascript, with the help of jQuery, Bootstrap, Moment.js, and OpenIconic libraries.
+### Assembled with CSS, HTML, and Javascript, with the help of jQuery, jQueryUI, Bootstrap, Moment.js, and OpenIconic libraries.
 
 
 Built off the starter code available [here.](https://github.com/coding-boot-camp/super-disco)
 
 A day planner that allows you to set up daily plans and track your progress on them, as well as how they relate to the current time of day.
+Also supports a calendar which allows you to make plans for days to come, as well as view old plans for days that have already gone.
 
 Live deployment available at: https://helpvisa.github.io/work_scheduler_05challenge_scs/
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 //**** query elements on page ****//
-var dateHeaderEl = $("#current-day");
 var plannerEl = $("#planner");
 
 
 //**** global variables ****//
 var currentDate = moment(); // call moment.js to get the current date in local time
+var onScreenDate = moment();
+var dayDisplay = currentDate.get("month") + "-" + currentDate.get("date"); // which day to display
 var currentPlans = [] // stores dynamically created plans (can be looped over to modify, save, load into, etc)
-var savedPlans = JSON.parse(localStorage.getItem("plans"));
+var savedPlans;
 
 
 //**** main body of code ****//
@@ -19,19 +20,59 @@ for (var i = 9; i < 18; i++) { // 9 - 5 workday!
     var currentEntry = createHourEntry(i);
     plannerEl.append(currentEntry);
 }
-// create empty array if no savedata could be loaded
-if (!savedPlans) {
-    savedPlans = []
-    updatePlans();
-}
-// otherwise load plans
-else {
+
+loadPlans();
+updatePlans();
+
+// add datepicker to current day header
+$(".jumbotron").on("click", "#current-day", function() {
+    // convert to input
+    var dateInput = $("<input>")
+        .addClass("form-control col-4 col-sm-3 col-md-2 m-auto")
+        .attr("id", "current-day")
+        .val(currentDate.format("ddd, MMM DD, h:mm a"));
+
+    // replace original element
+    $(this).replaceWith(dateInput);
+        
+    // enable datepicker
+    dateInput.datepicker();
+    // focus
+    dateInput.trigger("focus");
+});
+// update on close
+$(".jumbotron").on("change", "#current-day", function() {
+    // get new date
+    var date = $(this)
+        .val()
+        .trim();
+    
+    var momentDate = moment(date, "MM/DD/YYYY");
+
+    // make sure hour and minute are the same
+    momentDate.set("hour", currentDate.get("hour"));
+    momentDate.set("minute", currentDate.get("minute"));
+    onScreenDate = momentDate;
+
+    // convert to text
+    var textDate = momentDate.format("ddd, MMM DD, h:mm a");
+
+    // update day to display
+    dayDisplay = momentDate.get("month") + "-" + momentDate.get("date")
     loadPlans();
     updatePlans();
-}
+
+    var newHeaderEl = $("<p>");
+    newHeaderEl
+        .text(textDate)
+        .addClass("lead font-weight-bold")
+        .attr("id", "current-day");
+    
+    $(this).replaceWith(newHeaderEl);
+});
 
 // check for click events on the hourly timeslots (for editing)
-$(".plan").on("click", "p", function () {
+$(".plan").on("click", "p", function() {
     var text = $(this)
         .text()
         .trim();
@@ -40,11 +81,11 @@ $(".plan").on("click", "p", function () {
         .addClass("form-control")
         .val(text);
 
-    $(this).replaceWith(textInput)
+    $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
 // save new updated text on click of save button
-$(".save").on("click", function () {
+$(".save").on("click", function() {
      // get the parent element's id
     var id = $(this)
         .closest(".timeslot")
@@ -75,9 +116,11 @@ $(".save").on("click", function () {
 // update date header at top of page
 function updateHeader() {
     currentDate = moment(); // update moment
-    var day = currentDate.format("ddd, h:mm a");
+    // make sure on screen date matches w chosen date on calendar
+    onScreenDate.set("hour", currentDate.get("hour"));
+    onScreenDate.set("minute", currentDate.get("minute"));
 
-    dateHeaderEl.text(day);
+    $(".jumbotron").find("#current-day").text(onScreenDate.format("ddd, MMM DD, h:mm a"));
 
     updatePlans(); // update plans here too, since it will be called every ten seconds
 }
@@ -142,6 +185,7 @@ function createHourEntry(rawTime) {
 function updatePlans() {
     // get current hour time
     var currentHour = parseInt(moment().format("H")); // convert to int for easy comparisons
+    var actualCurrentDay = currentDate.get("month") + "-" + currentDate.get("date");
 
     // loop over current plans and alter colour
     for (var i = 0; i < currentPlans.length; i++) {
@@ -151,14 +195,42 @@ function updatePlans() {
         var pEl = $("#hour-" + planHour);
 
         // set colour
-        if (currentHour < planHour) {
-            pEl.addClass("future");
-        }
-        else if (currentHour === planHour) {
-            pEl.addClass("present");
+        if (actualCurrentDay === dayDisplay) {
+            if (currentHour < planHour) {
+                pEl.removeClass("past present");
+                pEl.addClass("future");
+            }
+            else if (currentHour === planHour) {
+                pEl.removeClass("past future");
+                pEl.addClass("present");
+            }
+            else {
+                pEl.removeClass("present future");
+                pEl.addClass("past");
+            }
         }
         else {
-            pEl.addClass("past");
+            // split dates into two parts (month / day) to get past / future
+            var displayDateArray = dayDisplay.split("-");
+            var currentDateArray = actualCurrentDay.split("-");
+            if (displayDateArray[0] === currentDateArray[0]) { // same month; find day
+                if (displayDateArray[1] < currentDateArray[1]) { // in past
+                    pEl.removeClass("present future");
+                    pEl.addClass("past");
+                }
+                else { // future (would not get this far if it was present day)
+                    pEl.removeClass("past present");
+                    pEl.addClass("future");
+                }
+            }
+            else if (displayDateArray[0] < currentDateArray[0]) { // past month
+                pEl.removeClass("present future");
+                pEl.addClass("past");
+            }
+            else { // future month
+                pEl.removeClass("past present");
+                pEl.addClass("future");
+            }
         }
     }
 }
@@ -172,23 +244,76 @@ function savePlan(id, text) {
         }
     }
 
+    if (savedPlans.length === 0) { // if no days are stored, create an index for the current day
+        var dayObj = {
+            day: dayDisplay,
+            plans: []
+        }
+        savedPlans.push(dayObj)
+        savedPlans[savedPlans.length - 1].plans = [].concat(currentPlans);
+    }
+    else {
+        var foundDay = false;
+        for (var i = 0; i < savedPlans.length; i++) { // is this day already indexed? let's find it
+            if (savedPlans[i].day === dayDisplay) { // it is!
+                savedPlans[i].plans = [].concat(currentPlans); // create copy of current plans to store in this slot
+                foundDay = true;
+                break; // exit the loop
+            }
+        }
+        if (!foundDay) {
+            // for loop completed without breaking, hence this day was not found, so let's store it
+            var dayObj = {
+                day: dayDisplay,
+                plans: []
+            }
+            savedPlans.push(dayObj); // create index
+            savedPlans[savedPlans.length - 1].plans = [].concat(currentPlans); // tack plans on the end
+        }
+    }
+
     // update plans list in localStorage
-    localStorage.setItem("plans", JSON.stringify(currentPlans));
+    localStorage.setItem("plans", JSON.stringify(savedPlans));
 }
 
 // load saved plan data into locally stored plan data
 function loadPlans() {
+    savedPlans = JSON.parse(localStorage.getItem("plans"));
+    if (!savedPlans) {
+        savedPlans = []
+    }
+
+    var loadedPlans = null; // temp variable to store which plans are being loaded
+    for (var i = 0; i < savedPlans.length; i++) { // loop through the indices of the saved plans
+        if (savedPlans[i].day === dayDisplay) { // we found the right day
+            loadedPlans = [].concat(savedPlans[i].plans); // let's load it
+        }
+    }
+
+    if (!loadedPlans) { // no index found for today; let's back out of the function
+        for (var i = 0; i < currentPlans.length; i++) {
+            currentPlans[i].text = "No plans.";
+            var pEl = $("#hour-" + currentPlans[i].id).find("p");
+            pEl.text(currentPlans[i].text); // set element text
+            pEl.addClass("font-italic text-secondary");
+        }
+        return;
+    }
+
     for (var i = 0; i < currentPlans.length; i++) { // iterate through list and pull id
         var id = currentPlans[i].id;
         // loop through loaded plan table and find match
-        // slower nested loop, but using id is not reliant on the two being in the same order, and load is only performed once
-        for (var l = 0; l < savedPlans.length; l++) {
-            if (savedPlans[l].id === id) { // match found
-                currentPlans[i].text = savedPlans[i].text; // set text
+        // slower nested loop, but using id is not reliant on the two being in the same order
+        for (var l = 0; l < loadedPlans.length; l++) {
+            if (loadedPlans[l].id === id) { // match found
+                currentPlans[i].text = loadedPlans[i].text; // set text
                 var pEl = $("#hour-" + id).find("p");
                 pEl.text(currentPlans[i].text); // set element text
                 if (pEl.text().replace(".", "") != "No plans") {
                     pEl.removeClass("font-italic text-secondary");
+                }
+                else {
+                    pEl.addClass("font-italic text-secondary");
                 }
             }
         }
